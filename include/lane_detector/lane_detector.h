7b2ec672ef9954be0fda66d35c099ee15a985467
pses <hamacher.nils@gmail.com>
2018-11-18 15:02:20 +0100

    first commit

---
+++ include/lane_detector/lane_detector.h
@@ -0,0 +1,13 @@
+/**
+ * @file "lane_detector/lane_detector.h"
+ * @brief Header file for the LANE_DETECTOR class.
+ *
+*/
+
+#ifndef LANE_DETECTOR_H
+#define LANE_DETECTOR_H
+
+#include <math.h>
+#include <string>
+#include <sstream>
+#include <boost/math/special_functions/sign.hpp>
---
+++ src/lane_detector_node.cpp
@@ -0,0 +1,12 @@
+//#include "lane_detector.h"
+
+
+
+
+int main(int argc, char** argv)
+{
+  //ros::init(argc, argv, "lane_detector");
+  //ros::NodeHandle nh;
+  //catchUnixSignals({SIGQUIT, SIGINT, SIGTERM, SIGHUP});
+  return 0;//a.exec();
+}
