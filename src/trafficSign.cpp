0ecf8afea6212a290c82298b6bf9db6e41752e88
DBadLuck <daniel.pech@stud.tu-darmstadt.de>
2018-11-24 17:44:10 +0100

    main with anforderung erstellt

---
+++ src/trafficSign.cpp
@@ -0,0 +1,20 @@
+//include Files
+
+
+
+/**
+ *@brief main runs the main algorithm of the traffic sign recognition.
+ *@brief It reads images from the Webcam
+ *@brief A SVM is trained and used to classify new signs features found by the robot.
+ *@brief The result of the detection (type of sign, size) are published using a custom message.
+ *@param argc is the number of arguments.
+ *@param argv is the arguments characters array.
+ *@return 0
+ */
+int main(int argc, char **argv) {
+
+
+
+
+}
+
