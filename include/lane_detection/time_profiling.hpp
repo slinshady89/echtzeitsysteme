123906f9fca4e437e77f79844be27d0f82ccae6a
Frederic Jacob <frederic_jacob@web.de>
2018-12-28 00:18:16 +0100

    [time_profiling.hpp] Fixed errors in timer directives.

--- include/lane_detection/time_profiling.hpp
+++ include/lane_detection/time_profiling.hpp
@@ -5,11 +5,11 @@
 #define USE_TIMER
 
 #ifdef USE_TIMER
-    #define TIMER_INIT ros::Time startTime; ros::Time endTime; ros::Duration dur; float time;
+    #define TIMER_INIT ros::Time startTime; ros::Time endTime; ros::Duration dur; float time; int millis;
     #define TIMER_START startTime = ros::Time::now();
     #define TIMER_STOP endTime = ros::Time::now();
     #define TIMER_EVALUATE(message) dur = endTime - startTime; \
-                                int millis = dur.toNSec / 1000000; \
+                                millis = dur.toNSec() / 1000000; \
                                 ROS_INFO("TIME: message ---> %f ms", millis);
 
 
