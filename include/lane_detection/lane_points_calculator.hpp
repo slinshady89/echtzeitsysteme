9da61b5d3da2f3956833afb95685c8b5ee949df7
Frederic Jacob <frederic_jacob@web.de>
2018-12-29 18:04:16 +0100

    Added new class [LanePointsCalculator] for the computing logic to determine trajectory / lane points.

---
+++ include/lane_detection/lane_points_calculator.hpp
@@ -0,0 +1,21 @@
+#ifndef LANE_POINTS_CALCULATOR_HPP_
+#define LANE_POINTS_CALCULATOR_HPP_
+
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+
+// TODO: make this class singleton?
+class LanePointsCalculator {
+    public:
+        LanePointsCalculator() {};
+        ~LanePointsCalculator() {};
+
+    private:
+
+};
+
+
+
+
+#endif // LANE_POINTS_CALCULATOR_HPP_
---
+++ src/lane_detection/lane_points_calculator.cpp
@@ -0,0 +1,1 @@
+#include "lane_detection/lane_points_calculator.hpp"
