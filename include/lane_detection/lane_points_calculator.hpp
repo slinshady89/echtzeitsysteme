1a0aaf2d17c41b9a15fc19d974236acc87ce16cb
Frederic Jacob <frederic_jacob@web.de>
2018-12-31 00:59:42 +0100

    [LanePointsCalculator] Added method to find the orthogonal point to a line segment defined by two points and with a given distance, either to the left or right

--- include/lane_detection/lane_points_calculator.hpp
+++ include/lane_detection/lane_points_calculator.hpp
@@ -5,11 +5,15 @@
 
 using namespace cv;
 
+enum Direction { LEFT, RIGHT };
+
 // TODO: make this class singleton?
 class LanePointsCalculator {
     public:
         LanePointsCalculator() {};
         ~LanePointsCalculator() {};
+
+        Point2i orthoPoint(Point2i start, Point2i end, Direction dir, double dist);
 
     private:
 
--- src/lane_detection/lane_points_calculator.cpp
+++ src/lane_detection/lane_points_calculator.cpp
@@ -1,1 +1,22 @@
 #include "lane_detection/lane_points_calculator.hpp"
+
+
+Point2i LanePointsCalculator::orthoPoint(Point2i start, Point2i end, Direction dir, double dist) {
+    Point2i middle = 0.5 * (start + end);
+    Point2i startToEnd = end - start;
+
+    int rotatedX, rotatedY;
+    switch (dir) {
+        case LEFT:
+            rotatedX = -startToEnd.y;
+            rotatedY = startToEnd.x;
+            break;
+        case RIGHT:
+            rotatedX = startToEnd.y;
+            rotatedY = -startToEnd.x;
+            break;
+    }
+    double length = norm(startToEnd);
+    Point2d normedToMiddle = (1.0/length) * Point2d(rotatedX, rotatedY);
+    return middle + (Point2i)(dist * normedToMiddle);
+}
