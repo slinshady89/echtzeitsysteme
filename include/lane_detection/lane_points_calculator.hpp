5703ef038bf09693a7caf2da084971e080bdfdb8
Frederic Jacob <frederic_jacob@web.de>
2018-12-31 03:41:19 +0100

    [LanePointsCalculator] Added method for calculating multiple trajectory points for given image height offsets (three different modes possible).

--- include/lane_detection/lane_points_calculator.hpp
+++ include/lane_detection/lane_points_calculator.hpp
@@ -2,10 +2,12 @@
 #define LANE_POINTS_CALCULATOR_HPP_
 
 #include <opencv2/opencv.hpp>
+#include "lane_detection/image_processor.hpp"
 
 using namespace cv;
 
 enum Direction { LEFT, RIGHT };
+enum SearchMode { FIXED_DIST, MIDDLE, MIDDLE_FALLBACK }
 
 // TODO: make this class singleton?
 class LanePointsCalculator {
@@ -14,6 +16,15 @@ class LanePointsCalculator {
         ~LanePointsCalculator() {};
 
         Point2i orthoPoint(Point2i start, Point2i end, Direction dir, double dist);
+        Point2i innerLanePoint(Direction searchDir, int row, ImageProcessor& proc);
+        /*  Searches in the given rows for lane pixels and calculates trajectory points from these.
+            searchDir specifies from which side the search is done (LEFT for finding the right, RIGHT for finding the left lane first) 
+            searchMode: 
+                FIXED_DIST calculates all points with a fixed distance from the first found lane
+                MIDDLE only finds points for rows where both lanes can be detected
+                MIDDLE_FALLBACK prefers calculating middle points and uses the distance as a fallback
+        */
+        std::vector<Point2i> trajPointsSimple(int *rows, int rows_size, Direction searchDir, SearchMode searchMode, int distance, ImageProcessor& proc);
 
     private:
 
--- src/lane_detection/lane_points_calculator.cpp
+++ src/lane_detection/lane_points_calculator.cpp
@@ -20,3 +20,43 @@ Point2i LanePointsCalculator::orthoPoint(Point2i start, Point2i end, Direction d
     Point2d normedToMiddle = (1.0/length) * Point2d(rotatedX, rotatedY);
     return middle + (Point2i)(dist * normedToMiddle);
 }
+
+
+Point2i LanePointsCalculator::innerLanePoint(Direction searchDir, int row, ImageProcessor& proc) {
+    Point2i p;
+    switch (searchDir) {
+        case LEFT:
+            p = proc.firstMatchFromRight(row);
+            return proc.lastMatchLeft(p);
+        case RIGHT:
+            p = proc.firstMatchFromLeft(row);
+            return proc.lastMatchRight(p);
+    }
+}
+
+std::vector<Point2i> LanePointsCalculator::trajPointsSimple(int *rows, int rows_size, Direction searchDir, SearchMode searchMode, int distance, ImageProcessor& proc) {
+    std::vector<Point2i> results;
+    Point2i firstLanePoint, secondLanePoint; 
+    for (int i=0; i<rows_size; i++) {
+        firstLanePoint = innerLanePoint(searchDir, rows[i], proc);
+        if (searchMode!=FIXED_DIST) {
+            Direction opposite = (searchDir==LEFT) ? RIGHT : LEFT;
+            secondLanePoint = innerLanePoint(opposite, rows[i], proc);
+            // check if the two points are really from different lanes
+            if ((searchDir==LEFT && firstLanePoint.x>secondLanePoint.x) || (searchDir==RIGHT && firstLanePoint.x<secondLanePoint.x)) {
+                results.push_back(0.5 * (firstLanePoint+secondLanePoint));
+                continue;
+            }
+            if (searchMode==MIDDLE) {
+                continue;
+            }
+        }
+        int offset = (searchDir==LEFT) ? -distance : distance;
+        // only add point to the results if it is still in image range
+        int x = firstLanePoint.x + offset;
+        if (x>=0 && x<proc.getImage().cols) {
+            results.push_back(Point2i(x,firstLanePoint.y));
+        }
+    }
+    return results;
+}
