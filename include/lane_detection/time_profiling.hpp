12102cae1c4f089543ff9663cd7afa0fb93fd182
Frederic Jacob <frederic_jacob@web.de>
2019-01-24 17:46:00 +0100

    docs(timer): add some docs

--- include/lane_detection/time_profiling.hpp
+++ include/lane_detection/time_profiling.hpp
@@ -1,6 +1,14 @@
 #ifndef TIME_PROFILING_HPP
 #define TIME_PROFILING_HPP
 
+/**
+ * These macros can be used to measure the duration of code fragments in a ROS program.
+ * TIMER_INIT must be called exactly once in a scope before the other commands to setup
+ * measurement variables.
+ *
+ * If USE_TIMER is not defined, all macros will have no effect and just removed by the preprocessor.
+ */
+
 // comment this line to not use time profiling
 #define USE_TIMER
 
@@ -8,6 +16,7 @@
     #define TIMER_INIT ros::Time startTime; ros::Time endTime; float millis;
     #define TIMER_START startTime = ros::Time::now();
     #define TIMER_STOP endTime = ros::Time::now();
+    // prints the measured time elapsed between TIMER_START and TIMER_STOP to the ROS console
     #define TIMER_EVALUATE(message) millis = (endTime.toSec() - startTime.toSec()) * 1000.0; \
                                     ROS_INFO("TIME: [%s] ---> %f ms", #message, millis);
 
