f9dfd6516a02075e40ee2840921255f5a74e1d7f
Frederic Jacob <frederic_jacob@web.de>
2019-01-31 15:11:35 +0100

    feat: add constants folder and file

---
+++ include/constants/constants.h
@@ -0,0 +1,13 @@
+//
+// Created by frederic on 31.01.19.
+//
+
+#ifndef PROJECT_CONSTANTS_H
+#define PROJECT_CONSTANTS_H
+
+/* webcam configuration */
+#define WEBCAM_WIDTH 800
+#define WEBCAM_HEIGHT 480
+
+
+#endif //PROJECT_CONSTANTS_H
