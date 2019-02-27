940f1ac587587020a6ac8c4d3d585ad4f7214697
slinshady <hamacher.nils@gmail.com>
2019-02-27 18:13:45 +0100

    changes to Ultrasonic sensors that zero distance is discarded

--- src/trajectory_planner/controller.cpp
+++ src/trajectory_planner/controller.cpp
@@ -78,11 +78,16 @@ bool CController::ctrlLoop(sensor_msgs::Range _rangeUSL, sensor_msgs::Range _ran
     double minDist = getUsMinDist();
     if (_rangeUSF.range < 1.0)        ROS_INFO("US front: %f",(float) _rangeUSF.range);     
     if (_rangeUSL.range < 1.0)        ROS_INFO("US left: %f",(float) _rangeUSL.range);     
-    if (_rangeUSR.range < 1.0)        ROS_INFO("US right: %f",(float) _rangeUSR.range); 
-    if(_rangeUSL.range <= minDist)      return false;
-    if(_rangeUSR.range <= minDist)      return false;
-    if(_rangeUSF.range <= minDist)      return false;
-    return true;
+    if (_rangeUSR.range < 1.0)        ROS_INFO("US right: %f",(float) _rangeUSR.range);
+  if (_rangeUSL.range <= minDist) {
+    if (_rangeUSL.range != 0) return false;
+  }
+  if (_rangeUSR.range <= minDist) {
+    if (_rangeUSR.range != 0) return false;
+  }
+  if (_rangeUSF.range <= minDist) {
+    if (_rangeUSF.range != 0) return false;
+  }
 };
 
 bool CController::ctrlLoop(double range_usl, double range_usr, double range_usf){
